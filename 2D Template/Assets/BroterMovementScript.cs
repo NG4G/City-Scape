0adccb6c0d3e6ee78349d93fe4fbcb4d8a531d12
MUKILTEO\1725203 <1>
2025-09-17 08:44:13 -0700

    Player movement

---
+++ 2D Template/Assets/BroterMovementScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class BroterMovementScript : MonoBehaviour
+{
+    public float speed;
+    public float jumpHeight;
+
+    public Rigidbody2D rb2d;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
