3e9d74be94b6d3dc0c51165d652bcb79eb128353
MUKILTEO\1725203 <1>
2025-09-18 10:15:58 -0700

    Worked on enemy ai

---
+++ 2D Template/Assets/enemyPatrol.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class enemyPatrol : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
